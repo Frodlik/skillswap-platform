cc0d723be14434e6a04b2501175ebb72a01d79da
Vadym Zakletskyi <zakletskiy.vadim@kpi.ua>
2026-05-01 00:43:23 +0300

    feat(observability): integrate Prometheus and Grafana for metrics monitoring

--- matching-service/src/main/java/com/skillswap/matchingservice/config/RestClientConfig.java
+++ matching-service/src/main/java/com/skillswap/matchingservice/config/RestClientConfig.java
@@ -1,30 +1,39 @@
 package com.skillswap.matchingservice.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
 
+    @Bean("restClientBuilder")
+    @Primary
+    RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
+
     @Bean
     @LoadBalanced
-    RestClient.Builder lbRestClientBuilder() {
+    @Qualifier("loadBalancedRestClientBuilder")
+    RestClient.Builder loadBalancedRestClientBuilder() {
         return RestClient.builder();
     }
 
     @Bean("userRestClient")
     RestClient userRestClient(
-            @LoadBalanced RestClient.Builder builder,
-            @Value("${services.user-service.base-url:http://user-service}") String baseUrl) {
+            @Qualifier("loadBalancedRestClientBuilder") RestClient.Builder builder,
+            @Value("${services.user-service.base-url}") String baseUrl) {
         return builder.baseUrl(baseUrl).build();
     }
 
     @Bean("skillRestClient")
     RestClient skillRestClient(
-            @LoadBalanced RestClient.Builder builder,
+            @Qualifier("loadBalancedRestClientBuilder") RestClient.Builder builder,
             @Value("${services.skill-service.base-url:http://skill-service}") String baseUrl) {
         return builder.baseUrl(baseUrl).build();
     }
